3f5de73ff814b4f50cfedf9f316a14731423b374
Kevin Yan <yan@mac.local>
2020-07-07 20:56:13 +1000

    add healthcheck

--- routes/musician.js
+++ routes/musician.js
@@ -6,7 +6,7 @@ const schema = require('../store/schema');
 
 // healthcheck
 router.get('/health', (req, res) => {
-  res.status('200').send("Status: ok!");
+  res.status('200').send("Status: code 200, ok!");
 });
 
 // retrieve all musicians from data store
